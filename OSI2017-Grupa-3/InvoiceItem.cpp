be806a4250e85db53663bfa29c4a50224c6f0e1b
Boris Lazic <lazicb65@yahoo.com>
2017-12-16 11:50:48 +0100

    Inicijalni commit za InvoiceItem klasu.

---
+++ OSI2017-Grupa-3/InvoiceItem.cpp
@@ -0,0 +1,3 @@
+#include "InvoiceItem.h"
+
+InvoiceItem::InvoiceItem(string article, float price, float quantity) :article(article), price(price) {};
---
+++ OSI2017-Grupa-3/InvoiceItem.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+#include "Message.h"
+
+class InvoiceItem
+{
+	string article;
+	float price, quantity;
+public:
+	InvoiceItem(string, float, float);//артикал, цијена, квантитет
+	float getTotalPrice();
+	string hasErrors();
+	string getArticle();
+	float getPrice();
+	float getQuantity();
+};
+
