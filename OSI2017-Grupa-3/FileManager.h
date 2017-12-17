a2c9c0f70eef561f1d68302e3dbd2f18006c3c4d
Boris Lazic <lazicb65@yahoo.com>
2017-12-17 18:17:54 +0100

    Re-commit FileManager klase zbog prethodnog konflikta

---
+++ OSI2017-Grupa-3/FileManager.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <string>
+#include <iostream>
+#include <vector>
+#pragma once
+class FileManager
+{
+private:
+	static std::string getexepath();
+public:
+	static std::vector<std::string> GetPathsWithExtension(std::string ext, std::string relativePath);
+};
