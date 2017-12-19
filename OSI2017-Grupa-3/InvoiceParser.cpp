a8c8c40723e0cfeb2e440fca3b9bf05bea0b4e50
Korisnik <Korisnik@Korisnik-PC>
2017-12-19 16:23:09 +0100

    Pokusaj pravljenja InvoiceParse klase.Zavrseno tek InvoiceParse detekcija formata i ucitavanje formata 1.Potrebno je tu dosta sredjivanja koda al' Trivundza i ja smo nesto napravili.Ako se ispostavi da radi za format 1,lako ce raditi sa istom idejom za ostale.

---
+++ OSI2017-Grupa-3/InvoiceParser.cpp
@@ -0,0 +1,136 @@
+#include "InvoiceParser.h"
+#include <sstream>
+
+using std::stringstream;
+
+string InvoiceParser::parseFromText(Invoice &invoice, string invoiceText)
+{
+	switch (detectFormat(invoiceText))
+	{
+	case 1: return parseFormat1(invoice, invoiceText);
+	case 2: return parseFormat2(invoice, invoiceText);
+	case 3: return parseFormat3(invoice, invoiceText);
+	case 4: return parseFormat4(invoice, invoiceText);
+	case 5: return parseFormat5(invoice, invoiceText);
+	default: return string(); /*Ne znam sta treba da vrati ali skontacemo.*/
+	};
+}
+
+int InvoiceParser::detectFormat(string invoiceText)
+{
+	if (invoiceText[0] == 'K')/*Format 1 ili 4.*/
+	{
+		int newPosition = moveRows(3, 0, invoiceText);/*Pomjeramo se u red sa naslovom racuna.*/
+		while (invoiceText[newPosition++] == ' ');
+		if (invoiceText[newPosition] == 'R')/*Detektovan racun format 1.*/
+			return 1;
+		else
+			return 4;/*Detektovan racun format 4.*/
+	}
+	else if (invoiceText[0] == 'O')/*Format 2 ili 3.*/
+	{
+		int newPosition = moveRows(2, 0, invoiceText);
+		if (invoiceText[newPosition] == '\n')
+			return 2;/*Detektovan format 2.*/
+		else
+			return 3;/*Detektovan format 3.*/
+	}
+	else if (invoiceText[0] == 'S')
+		return 5;/*Detektovan format 5.*/
+	else
+		return 0;/*Pogresan format.*/
+}
+
+string InvoiceParser::parseFormat1(Invoice &invoice, string invoiceText)
+{
+	int position = 0;
+	char c;
+	string stringValue;
+
+	string line = getLineOfText(position, invoiceText);
+	std::stringstream stringStream(line);
+	stringStream >> stringValue >> c >> invoice.buyer;/*Unosi ime kupca.*/
+
+	line = getLineOfText(position, invoiceText);/*Unosi datum.*/
+	stringstream stringStream1(line);
+	stringStream1 >> stringValue >> c >> invoice.date;
+
+	line=getLineOfText(position, invoiceText);
+	line=getLineOfText(position, invoiceText);
+	line=getLineOfText(position, invoiceText);
+	line=getLineOfText(position, invoiceText);/*Pomjeramo se na pocetak liste artikala.*/
+
+	int savePosition = position, countItems = 0;
+	while (line[0] != '-')
+	{
+		line = getLineOfText(position, invoiceText);
+		countItems++;
+	}
+	position = savePosition;
+	invoice.items = new InvoiceItem[countItems];/*Cuvamo tu poziciju i brojimo koliko ima artikala.*/
+
+	string value1, value2;
+	for (int j = 0; j < countItems; j++)
+	{
+		line = getLineOfText(position, invoiceText);
+		stringstream stringStream2(line);
+		stringStream2 >> value1 >> value2 >> c >> invoice.items[j].quantity >> c >> invoice.items[j].price >> c >> invoice.items[j].totalPrice;
+		invoice.items[j].article = value1 + value2;
+	}/*Unosimo artikal po artikal.Sabiramo dva stringa jer smo glupi da to odradimo odvojeno.*/
+
+	invoice.numItems = countItems;
+	double PDV, price, priceToPay;
+
+	line = getLineOfText(position, invoiceText);
+	line = getLineOfText(position, invoiceText);/*Sklanjamo red crtica i uzimamo red cijene i ucitavamo je.*/
+	stringstream stringStream3(line);
+	stringStream3 >> stringValue >> c >> price;
+
+	line = getLineOfText(position, invoiceText);/*Uzimamo red pdv-a i ucitavamo ga.*/
+	stringstream stringStream4(line);
+	stringStream4 >> stringValue >> c >> PDV;
+
+	line = getLineOfText(position, invoiceText);
+	stringstream stringStream5(line);
+	stringStream5 >> stringValue >> c >>priceToPay; /*Uzimamo red ukupne cijene i ucitavamo je.*/
+
+	return string();/*Ne znamo sta treba da vrati ali saznacemo.*/
+}
+
+string InvoiceParser::parseFormat2(Invoice &invoice, string invoiceText)
+{
+	return string();
+}
+
+string InvoiceParser::parseFormat3(Invoice &invoice, string invoiceText)
+{
+	return string();
+}
+
+string InvoiceParser::parseFormat4(Invoice &invoice, string invoiceText)
+{
+	return string();
+}
+
+string InvoiceParser::parseFormat5(Invoice &invoice, string invoiceText)
+{
+	return string();
+}
+
+int InvoiceParser::moveRows(int rows, int position,string inputText)
+{
+	int count = 0, i;
+	for ( i = position; count < rows; i++)
+		if (inputText[i] == '\n')
+			count++;
+	return i;
+}
+
+string InvoiceParser::getLineOfText(int& position, string inputText)
+{
+	string result;
+	for (position; inputText[position] != '\n'; position++)
+		result.push_back(inputText[position]);
+	position++;
+	return result;
+}
---
+++ OSI2017-Grupa-3/InvoiceParser.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "Invoice.h"
+
+
+class InvoiceParser
+{
+public:
+	string parseFromText(Invoice &, string);
+private:
+	int detectFormat(string);
+
+	string parseFormat1(Invoice&, string);
+	string parseFormat2(Invoice&, string);
+	string parseFormat3(Invoice&, string);
+	string parseFormat4(Invoice&, string);
+	string parseFormat5(Invoice&, string);
+
+	int moveRows(int, int,string);
+	string getLineOfText(int&, string);
+};
+
