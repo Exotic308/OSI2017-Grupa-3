e693b1d3f1205363b8033bbfb5ec326db7993746
Boris Lazic <lazicb65@yahoo.com>
2018-01-19 16:30:53 +0100

    Dodati komentare, izbrisane suvisne funckije i biblioteke.

--- OSI2017-Grupa-3/InvoiceItem.cpp
+++ OSI2017-Grupa-3/InvoiceItem.cpp
@@ -1,13 +1,22 @@
 ﻿#include "InvoiceItem.h"
+#include "Message.h"
 #include <iostream>
+
+using std::cout;
+using std::endl;
 InvoiceItem::InvoiceItem(string article, float price, float quantity, float totalPrice):
 	article(article), price(price), quantity(quantity), totalPrice(totalPrice) {};
 
+/*Metoda koja kompozitno dobija ukupnu cijenu proizvoda.
+  Poziva se u funkciji za provjeru errora proizvoda.*/
 float InvoiceItem::getTotalPrice()
 {
 	return price*quantity;
 }
 
+/*Metoda koja provjerava da li postoje errori na proizvodu.
+  Konstruisana na nacin da se moze jednostavno upotrebljavati
+  sa metodama Message klase.*/
 string InvoiceItem::hasErrors()
 {
 	if (quantity <= 0 || price <= 0)
@@ -18,19 +27,21 @@ string InvoiceItem::hasErrors()
 		return "1Pravilan oblik racuna, unos je moguc";
 }
 
-using std::cout;
-using std::endl;
+/*Metoda za ispis osobina proizvoda.*/
 void InvoiceItem::print() {
 	cout << "Artikal: " << article << endl;
 	cout << "Cijena: " << price << endl;
 	cout << "Kolicina: " << quantity << endl;
 	cout << "Ukupna cijena: " << totalPrice << endl;
 	cout << "Izracunata cijena: " << getTotalPrice() << endl;
-	cout << "Erori: " << hasErrors() << endl;
+	cout << "Erori: " << Message::getMessage(hasErrors()) << endl;
 }
 
+/*Preklopljen operator poredjivanja jednakosti dva elementa.*/
 bool InvoiceItem::operator==(InvoiceItem &other)
 {
-	if (article == other.article&&price == other.price&&quantity == other.quantity&&totalPrice == other.totalPrice) return true;
+	if (article == other.article && price == other.price && 
+		quantity == other.quantity && totalPrice == other.totalPrice) 
+		return true;
 	return false;
 }
