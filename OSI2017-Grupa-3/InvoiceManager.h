81a69715cf9785ba0a6561aee6420d8b13bfc62e
YouFaceJaraxxus <34375459+YouFaceJaraxxus@users.noreply.github.com>
2018-01-19 15:52:11 +0100

    Komentari na metode Message i InvoiceManager

--- OSI2017-Grupa-3/InvoiceManager.h
+++ OSI2017-Grupa-3/InvoiceManager.h
@@ -8,15 +8,15 @@ using std::vector;
 
 class InvoiceManager
 {
-	bool alreadyExists(Invoice&);
+	bool alreadyExists(Invoice&); //provjera vec postojuceg racuna u nizu
 public:
-	static InvoiceManager *Instance;
-	static string getStringFromFile(const char * path);
-	vector<Invoice> invoice_array;
-	vector<Invoice> filterValid();
-	void scanForNewInvoices();
-	Invoice loadFromFile(const char*);
-	InvoiceManager();
-	~InvoiceManager();
+	static InvoiceManager *Instance; //staticka instanca (singleton) InvoiceManager-a
+	static string getStringFromFile(const char * path); //funkcija koja ucitava sadrzaj datoteke u obliku string-a
+	vector<Invoice> invoice_array; //niz racuna
+	vector<Invoice> filterValid(); //funkcija za filtriranje validnih racuna u nizu
+	void scanForNewInvoices(); //funkcija za skeniranje novih racuna u okruzenju aplikacije
+	Invoice loadFromFile(const char*); //funkcija koja ucitava podatak iz odgovarajuceg fajla te ga cuva u obliku Invoice-a
+	InvoiceManager(); //konstruktor
+	~InvoiceManager(); //destruktor
 };
 
--- OSI2017-Grupa-3/Message.h
+++ OSI2017-Grupa-3/Message.h
@@ -5,10 +5,10 @@ using std::string;
 
 class Message
 {
-	static void deductFirst(string&);
-	static bool isGoodFormat(string);
+	static void deductFirst(string&); //funkcija koja brise prvi znak string-a
+	static bool isGoodFormat(string); //funkcija za provjeru formata poruke
 public:
-	static bool isSuccess(string);
-	static string getMessage(string);
+	static bool isSuccess(string); //funkcija za provjeru da li je poruka o uspjehu ili neuspjehu
+	static string getMessage(string); //funkcija koja provjerava uspjeh poruke i vrsi njeno formatiranje u konacni oblik
 };
 
